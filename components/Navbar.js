d4b4d7501dd15ff733f7ac1e7d424fb9f7c427d6
Danny Ohana <danny.ohana@gmail.com>
2020-12-06 22:05:17 -0500

    navbar animatepresence for text and search input field

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,59 @@
 import { SearchCommand } from "./SearchCommand";
 import styles from "./styles/Navbar.module.css";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Navbar = (props) => {
   const { isActive } = props;
 
+  const collapsedVariants = {
+    opacity: 0,
+    transform: "translateX(-20px)",
+  };
+
+  const openVariants = {
+    opacity: 1,
+    transform: "translateX(0px)",
+  };
+
+  const collapsedVariants2 = {
+    opacity: 0,
+    transform: "translateX(20px)",
+  };
+
   return (
     <div className={styles.navbar}>
-      {!isActive ? (
-        <div className={styles.logoText}>Danny Ohana</div>
-      ) : (
-        <input
-          autoFocus
-          className={styles.navInput}
-          type="text"
-          placeholder="Filter"
-        />
-      )}
+      <AnimatePresence initial={false}>
+        {!isActive ? (
+          <motion.div
+            className={styles.logoText}
+            variants={{
+              collapsed: collapsedVariants,
+              open: openVariants,
+            }}
+            animate="open"
+            initial="collapsed"
+            exit="collapsed"
+            transition={{ duration: 0.33, ease: [0.04, 0.62, 0.23, 0.98] }}
+          >
+            Danny Ohana
+          </motion.div>
+        ) : (
+          <motion.input
+            autoFocus
+            className={styles.navInput}
+            type="text"
+            placeholder="Search"
+            variants={{
+              collapsed: collapsedVariants2,
+              open: openVariants,
+            }}
+            animate="open"
+            initial="collapsed"
+            exit="collapsed"
+            transition={{ duration: 0.33, ease: [0.04, 0.62, 0.23, 0.98] }}
+          ></motion.input>
+        )}
+      </AnimatePresence>
       <SearchCommand onSearchCommandExecute={props.onSearchCommandExecute} />
     </div>
   );
