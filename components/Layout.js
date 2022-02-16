c255541bd8b2db2ff98fe714e36d6d7698ede40e
Danny Ohana <danny.ohana@gmail.com>
2022-02-16 09:56:14 -0500

    modifications and removals

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,12 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import Navbar from "./Navbar";
-import SearchModal from "./SearchModal";
 
 const Layout = () => {
-  const [isModalActive, setIsModalActive] = useState(false);
-
-  function handleModalToggle() {
-    setIsModalActive((prevToggle) => !prevToggle);
-  }
-
   return (
     <div>
       <Head>
-        <title>Danny Ohana</title>
+        <title>Designer Engineer - dannyohana.com</title>
       </Head>
-      <Navbar onSearchCommandExecute={handleModalToggle} />
-      <SearchModal isActive={isModalActive} />
+      hello
     </div>
   );
 };
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,9 @@
-import { SearchCommand } from "./SearchCommand";
 import styles from "./styles/Navbar.module.css";
 
 const Navbar = (props) => {
   return (
     <div className={styles.navbar}>
       <div className={styles.logoText}>Danny Ohana</div>
-      <SearchCommand onSearchCommandExecute={props.onSearchCommandExecute} />
     </div>
   );
 };
