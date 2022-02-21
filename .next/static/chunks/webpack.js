0306e9a1745a5da5a3b6a18d12130011dc1986e2
Danny Ohana <danny.ohana@gmail.com>
2022-02-20 22:04:44 -0500

    floats

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cd2ed51af034d12d"; }
+/******/ 		__webpack_require__.h = function() { return "23acc44f36d82767"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,18 @@ import styles from "../styles.module.css";
 function HomePage() {
   return (
     <body className={styles.body}>
-      <p className={styles.p}>
+      <p className={styles.description}>
         <strong>Danny Ohana</strong> is the Director of Design at SportAI. After
-        graduating with a degree in Computer Science, he moved to Washington
-        D.C. to become a Frontend Engineer at GEICO. Now he’s back in Orlando
-        and is open to design and engineering jobs.
+        graduating with a Computer Science degree, he moved to Washington D.C.
+        to become a Frontend Engineer at GEICO. Now he’s back in Orlando and is
+        open to design and engineering jobs.
       </p>
       <a
         target="_blank"
-        className={styles.a}
         href="https://dannyohana.notion.site/1dd82f4365844b1fa4f9f278779715c2?v=308033fb2d8a4f878d0809a901db5c33"
       >
-        <p>
-          View his portfolio <span className={styles.orange}>{"->"}</span>
+        <p className={styles.portfolioButton}>
+          View his portfolio <span>{"->"}</span>
         </p>
       </a>
     </body>
