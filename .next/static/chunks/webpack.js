aba96ae3d10602e3d51013bda91f8307da1b7b24
Danny Ohana <danny.ohana@gmail.com>
2022-02-27 12:22:42 -0500

    favicon

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3583488c1858d85"; }
+/******/ 		__webpack_require__.h = function() { return "4f3d19b8ee5b03a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,11 @@ function HomePage() {
     <html>
       <head>
         <title>Danny Ohana</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={styles.body}>
         <p className={styles.description}>
