9e9e26ba1b600ca673ae1a4281f29975c6ab6b8c
Danny <danny.ohana@gmail.com>
2026-02-12 13:26:31 -0500

    adding stack json

---
+++ data/stack.js
@@ -0,0 +1,231 @@
+export const stack = [
+  {
+    name: "Apple Notes",
+    description: "I keep it simple.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Apple Reminders",
+    description: "Again... simple.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Figma",
+    description: "Interface design and prototyping",
+    platforms: "macOS",
+  },
+  {
+    name: "Figma Slides",
+    description: "The most fluid slide deck software",
+    platforms: "macOS",
+  },
+  {
+    name: "Dia",
+    description: "My favorite browser on desktop",
+    platforms: "macOS",
+  },
+  {
+    name: "Safari",
+    description: "My favorite browser on mobile",
+    platforms: "iOS",
+  },
+  {
+    name: "iPhone 15 Pro",
+    description: "I'll probably upgrade once the foldable iPhone gets released",
+    platforms: "Physical",
+  },
+  {
+    name: "BMW 330i",
+    description: "2021. Just fun to drive.",
+    platforms: "Physical",
+  },
+  {
+    name: "Zed",
+    description:
+      "Started using this as my code editor of choice at the end of 2025. I like it's speed, simplicity, and Claude Code integration.",
+    platforms: "macOS",
+  },
+  {
+    name: "Xcode",
+    description: "A chore to use, but it's the best IDE for iOS development.",
+    platforms: "macOS",
+  },
+  {
+    name: "Spotify",
+    description: "Music, Podcasts",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Youtube",
+    description: "Videos, Podcasts",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Substack",
+    description: "My favorite newsletter platform",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Claude",
+    description:
+      "My latest AI assistant of choice. Though it seems to change like once a fiscal quarter.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Claude Code",
+    description: "The best coding agent for now. We'll see if it changes.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Letterboxd",
+    description: "Where I track the movies I watch.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Goodreads",
+    description: "Where I track the books I read.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Github",
+    description: "How would I build without it?",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Copilot",
+    description:
+      "The best, most well-designed, actually sticky piece of budgeting software I've ever used. It will take a lot to overthrow this king.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Apple Calendar",
+    description: "Where I keep my life outside of work organized.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Google Calendar",
+    description: "Where I keep my work life oragnized.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "itsycal",
+    description:
+      "The most minimal calendar app I use. It lives in the macOS menu bar.",
+    platforms: "macOS",
+  },
+  {
+    name: "Raycast",
+    description: "A necessity for macOS power users.",
+    platforms: "macOS",
+  },
+  {
+    name: "Xbox Series S",
+    description:
+      "Where I play Arc Raiders... I'm hooked. I might upgrade to an Xbox Series X, though because the Series S feels slow and crashes often.",
+    platforms: "Physical",
+  },
+  {
+    name: "Steam Deck",
+    description:
+      "Where I play indie games, although I don't use this much anymore. I was only hooked on it for about 6 months.",
+    platforms: "Physical",
+  },
+  {
+    name: "Apple Passwords",
+    description:
+      "I keep all of my passwords here. It doesn't everything I need.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Airpods Pro 3",
+    description: "The best earbuds.",
+    platforms: "Physical",
+  },
+  {
+    name: "Apple Watch Series 10",
+    description: "My fitness tracker.",
+    platforms: "Physical",
+  },
+  {
+    name: "Anker 3-in-1 Cube Charger",
+    description:
+      "The only charger I need for my phone, Airpods, and Apple Watch.",
+    platforms: "Physical",
+  },
+  {
+    name: "Loom",
+    description: "How I record videos to share with my coworkers..",
+    platforms: "macOS",
+  },
+  {
+    name: "Robinhood",
+    description: "My personal stock trading platform.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Fidelity",
+    description: "My retirement accounts platform.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Beast Blender",
+    description:
+      "A nice upgrade from a NutriBullet. I wish the cups were easier to clean, but everything about this screams high-quality industrial design.",
+    platforms: "Physical",
+  },
+  {
+    name: "Gifski",
+    description: "Make GIFS as fast as possible.",
+    platforms: "macOS",
+  },
+  {
+    name: "Midjourney",
+    description:
+      "My Midjourney subscription gets toggled on and off depending on whether a visual project I'm working on rears it's head.",
+    platforms: "macOS",
+  },
+  {
+    name: "CleanShot X",
+    description: "A mile better than the native macOS screenshot tool.",
+    platforms: "macOS",
+  },
+  {
+    name: "CleanShot X",
+    description: "A mile better than the native macOS screenshot tool.",
+    platforms: "macOS",
+  },
+  {
+    name: "Mobbin",
+    description:
+      "The greatest software design resource. A trove of beautiful app and site designs.",
+    platforms: "macOS",
+  },
+  {
+    name: "Cash App Taxes",
+    description:
+      "Literally the best tax processing software. Genuinely fun to use?",
+    platforms: "macOS",
+  },
+  {
+    name: "Mymind",
+    description: "Automatic link organization tool.",
+    platforms: "macOS, iOS",
+  },
+  {
+    name: "Jabra Evolve 80 UC",
+    description:
+      "Let's me take meetings from coffee shops with ZERO background noise for the people I'm talking to.",
+    platforms: "Physical",
+  },
+  {
+    name: "Trek Emonda ALR",
+    description: "My lightweight, aluminum road bike.",
+    platforms: "Physical",
+  },
+  {
+    name: "Strava",
+    description:
+      "Keeps track of my workouts, and inspires me to keep up with my more athletic friends.",
+    platforms: "iOS",
+  },
+];
